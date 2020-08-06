95cbe430f0a659b0fe508366052fa069230577d5
Leandro Mendez <leandro@enviopack.com>
2020-08-06 11:44:00 -0300

    asd

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,24 +1,9 @@
 import { combineReducers } from 'redux';
-
-const songsReducer = () => {
-    return [
-        { title: 'Welcome to The Jungle', duration: '6:66' },
-        { title: 'Africa', duration: '6:66' },
-        { title: 'Winds of Change', duration: '6:66' },
-        { title: 'Paranoid', duration: '6:66' },
-    ];
-};
-
-const selectedSongReducer = (selectedSong = null, action) => {
-    if (action.type === 'SONG_SELECTED') {
-        return action.payload;
-    }
-
-    return selectedSong;
-};
+import songsReducer from './songsReducer';
+import selectedSongReducer from './selectedSongReducer';
 
 //The keys on this objects are going to be they keys on our state object
-//Basicamente este combineReducers es el que arma el "state" son las keys y los resultados de lo que devuelven los reducers que le pasas a las keys
+//Basicamente este combineReducers es el que arma el "state" con las keys, y los resultados de lo que devuelven los reducers que le pasas a las keys
 export default combineReducers({
     songs: songsReducer,
     selectedSong: selectedSongReducer,
---
+++ src/reducers/songsReducer.js
@@ -0,0 +1,8 @@
+export default () => {
+    return [
+        { title: 'Welcome to The Jungle', duration: '7:77' },
+        { title: 'Africa', duration: '7:77' },
+        { title: 'Winds of Change', duration: '7:77' },
+        { title: 'Paranoid', duration: '7:77' },
+    ];
+};
