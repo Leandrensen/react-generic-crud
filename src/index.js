093e4a4c6baa8eabde4639a8319ab08e33f4fe7d
Leandro Mendez <leandro@enviopack.com>
2020-08-07 16:28:33 -0300

    Routing added + redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import { ConnectedRouter } from 'connected-react-router';
 
-import reducers from './reducers';
 import Routes from './routes';
 import store from './store';
 import history from './history';
